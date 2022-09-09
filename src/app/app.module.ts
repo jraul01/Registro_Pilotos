c145cbf31b1dfb94efbe48daf83ed72b4df322ec
unknown <raularcos67@gmail.com>
2022-09-09 17:11:22 -0600

    Primera Subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PilotosComponent } from './components/pilotos/pilotos.component';
+import { VuelosComponent } from './components/vuelos/vuelos.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PilotosComponent,
+    VuelosComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'pilotos', component: PilotosComponent},
+      {path: 'vuelos', component: VuelosComponent},
+      {path: '**', redirectTo: '/', pathMatch: 'full'},
+     // {path: '**', component: PageNotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
